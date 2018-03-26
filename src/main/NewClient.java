5267d07676e35d527eade4f6c329ccae77fac728
Vlad <ysc.vlad@gmail.com>
2018-03-26 12:44:10 +0300

    add new_client frame

--- src/main/NewClient.java
+++ src/main/NewClient.java
@@ -3,16 +3,21 @@ package main;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class NewClient extends JFrame {
     private int COUNT_COLUMNS = 30;
 
-    private JTextField  firstName = new JTextField(COUNT_COLUMNS),
-                        surname = new JTextField(COUNT_COLUMNS),
-                        secondName = new JTextField(COUNT_COLUMNS),
-                        birthday = new JTextField(COUNT_COLUMNS),
-                        phone = new JTextField(COUNT_COLUMNS),
-                        work = new JTextField(COUNT_COLUMNS);
+    private JTextField  fieldFirstName = new JTextField(COUNT_COLUMNS),
+                        fieldSurname = new JTextField(COUNT_COLUMNS),
+                        fieldSecondName = new JTextField(COUNT_COLUMNS),
+                        fieldBirthday = new JTextField(COUNT_COLUMNS),
+                        fieldPhone = new JTextField(COUNT_COLUMNS),
+                        fieldWork = new JTextField(COUNT_COLUMNS);
     private JLabel  labelName = new JLabel("Имя"),
                     labelSurname = new JLabel("Фамилия"),
                     labelSecondName = new JLabel("Отчество"),
@@ -40,19 +45,22 @@ public class NewClient extends JFrame {
         line.setBorder(border); line2.setBorder(border);
         add(line).setBounds(0,70,form_width,1);
         add(labelSurname).setBounds(90,90,200,40);
-        add(surname).setBounds(220,90,200,40);
+        add(fieldSurname).setBounds(220,90,200,40);
         add(labelName).setBounds(150,160,200,40);
-        add(firstName).setBounds(220,160,200,40);
+        add(fieldFirstName).setBounds(220,160,200,40);
         add(labelSecondName).setBounds(90,230,200,40);
-        add(secondName).setBounds(220,230,200,40);
+        add(fieldSecondName).setBounds(220,230,200,40);
         add(labelBirthday).setBounds(20,300,200,40);
-        add(birthday).setBounds(220,300,200,40);
+        add(fieldBirthday).setBounds(220,300,200,40);
         add(labelContactPhone).setBounds(90,370,200,40);
-        add(phone).setBounds(220,370,200,40);
+        add(fieldPhone).setBounds(220,370,200,40);
         add(labelWork).setBounds(30,440,200,40);
-        add(work).setBounds(220,440,200,40);
+        add(fieldWork).setBounds(220,440,200,40);
         add(line2).setBounds(0,500,form_width,1);
         add(button_finish).setBounds(220,510,200,40);
+
+        //кнопка зарегистрировать
+        buttonRegistrateAction();
     }
 
 
@@ -67,12 +75,12 @@ public class NewClient extends JFrame {
         labelTitle.setFont(fontTitle);
         line.setBackground(Color.BLACK);
 
-        firstName.setFont(fontField);
-        secondName.setFont(fontField);
-        surname.setFont(fontField);
-        birthday.setFont(fontField);
-        work.setFont(fontField);
-        phone.setFont(fontField);
+        fieldFirstName.setFont(fontField);
+        fieldSecondName.setFont(fontField);
+        fieldSurname.setFont(fontField);
+        fieldBirthday.setFont(fontField);
+        fieldWork.setFont(fontField);
+        fieldPhone.setFont(fontField);
 
         labelWork.setFont(fontLabel);
         labelContactPhone.setFont(fontLabel);
@@ -82,7 +90,59 @@ public class NewClient extends JFrame {
         labelSurname.setFont(fontLabel);
 
         button_finish.setFont(fontButton);
+        button_finish.setBackground(Color.GRAY);
     }
 
 
+    //кнопка зарегистрировать
+    private void buttonRegistrateAction() {
+        button_finish.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    FileWriter write = new FileWriter("E:\\temp_file\\clients.txt",true);
+                    String  name = fieldFirstName.getText(),
+                            secondName = fieldSecondName.getText(),
+                            surname = fieldSurname.getText(),
+                            birthday = fieldBirthday.getText(),
+                            work = fieldWork.getText(),
+                            phone = fieldPhone.getText();
+
+
+
+                    if (name.equals("") && secondName.equals("") && surname.equals("") && birthday.equals("")
+                            && phone.equals("") && work.equals("")) {
+                        button_finish.setBackground(Color.RED);
+                        try {
+                            Thread.sleep(1000);
+                        }
+                        catch (Exception eee){}
+                        button_finish.setBackground(Color.GRAY);
+                    }
+                    else {
+                        write.append(name+" | ");
+                        write.append(secondName+" | ");
+                        write.append(surname+" | ");
+                        write.append(birthday+" | ");
+                        write.append(work+" | ");
+                        write.append(phone+" |\n");
+                    }
+
+                    write.flush();
+
+                    fieldFirstName.setText("");
+                    fieldSecondName.setText("");
+                    fieldSurname.setText("");
+                    fieldBirthday.setText("");
+                    fieldWork.setText("");
+                    fieldPhone.setText("");
+
+                }
+                catch (IOException ee){
+                    System.out.println("Error");
+                }
+
+            }
+        });
+    }
 }
